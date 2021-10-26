21df1abdf2915e2279c15510817b99cc70e5c05e
WoahJo <ccnettacc@gmail.com>
2021-10-26 19:12:09 -0400

    Created the bones for js

---
+++ script.js
@@ -0,0 +1,9 @@
+let bookshelf = [];
+
+function Book() {
+    //TODO create a constructor that takes the input from the popup and creates an object
+}
+
+function toBookshelf() {
+    //TODO create a function that sends the created object to the bookshelf
+}
