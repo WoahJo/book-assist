f7d86a34abc091a8a7fcfe673760fecbd062fb52
WoahJo <ccnettacc@gmail.com>
2021-10-28 18:22:24 -0400

    Add functions that take input for obj constructor and add obj to array. Basic style/placeholders.

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 let bookshelf = [];
+const ftitle = document.getElementById('title'); 
+const fauthor = document.getElementById('author');
+const fpages = document.getElementById('pages');
+const fread = document.getElementById('read');
+const fsubmit = document.getElementById('submit-book');
+const form = document.getElementById('book-form');
 
-function Book() {
+function Book(title, author, pages, read) {
     //TODO create a constructor that takes the input from the popup and creates an object
+    this.title = title; 
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function toBookshelf() {
-    //TODO create a function that sends the created object to the bookshelf
+    //TODO create a function that sends the created object to the 
+    bookshelf.push(new Book(ftitle.value, fauthor.value, fpages.value, fread.checked));
+
 }
+
+fsubmit.addEventListener("click", function(e){
+    if(!fpages.value){
+        console.log('number please!');
+    }
+    else{toBookshelf(); form.reset();}
+});
