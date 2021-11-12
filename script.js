a1771df7546eb567f932cf4d88f9a11576d84d8b
WoahJo <ccnettacc@gmail.com>
2021-11-12 18:46:59 -0500

    added function to populate portion of stored books WIP. created element creation fnct

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ const fcancel = document.getElementById('close-book');
 const cardField = document.querySelector('.card-field');
 const duplicate = document.querySelector('.duplicate');
 const modal = document.querySelector('.modal');
+let strBookshelf;
+let getBookshelf;
+let parseBookshelf;
 
 function storageAvailable(type){
     let storage;
@@ -18,25 +21,24 @@ function storageAvailable(type){
         let x = 'test';
         storage.setItem(x, x);
         storage.removeItem(x);
+        return true;
     }
     catch(e){
         return e instanceof DOMException && (e.code === 22 || e.code ===1014 || e.name === 'QuotaExceededError' || e.name === 'NS_ERROR_DOM_QUOTA_REACHED') && (storage && storage.length !== 0);
     }
 }
 
+window.onload = function(){
+    if(storageAvailable('localStorage') && localStorage.length > 0){
+        alert(localStorage.length);
+        getBookshelf = localStorage.getItem('strBookshelf');
+        parseBookshelf = JSON.parse(getBookshelf);
+        console.log(parseBookshelf);
+        populateField();
+    }
+};
 
-function Book(title, author, pages, read) {
-    //TODO create a constructor that takes the input from the popup and creates an object
-    this.title = title, 
-    this.author = author,
-    this.pages = pages,
-    this.read = read
-}
-
-function toBookshelf() {
-    //TODO create a function that sends the created object to the 
-    bookshelf.unshift(new Book(ftitle.value, fauthor.value, fpages.value, fread.checked));
-    
+function createElements(){
     const newReadButton = document.createElement('button');
     const cardDiv = document.createElement('div');
     const btitleH1 = document.createElement('h1');
@@ -97,8 +99,114 @@ function toBookshelf() {
         bookshelf.splice(bookshelf[index], 1);
     });
 
+    if(storageAvailable('localStorage')){
+        strBookshelf = JSON.stringify(bookshelf);
+        localStorage.setItem('strBookshelf', strBookshelf);
+
+    }
+}
+
+function populateField(){
+    for(let i = 0; i < parseBookshelf.length; i++){
+        bookshelf.unshift(new Book(parseBookshelf[i]. title, parseBookshelf[i].author, parseBookshelf[i].pages, parseBookshelf[i].read));
+        const parseCard = document.createElement('div');
+        const parseH1 = document.createElement('h1'); 
+        const parseH2 = document.createElement('h2'); 
+        const parseP = document.createElement('p'); 
+        const parseRead = document.createElement('button');
+        const parseRemove = document.createElement('button');
+        
+
+        
+        parseH1.className = "btitle";
+        parseCard.className = "card";
+        parseH1.textContent = parseBookshelf[i].title;
+        parseCard.appendChild(parseH1);
+        cardField.appendChild(parseCard);
+
+    }       
+}
+
+function Book(title, author, pages, read) {
+    //TODO create a constructor that takes the input from the popup and creates an object
+    this.title = title, 
+    this.author = author,
+    this.pages = pages,
+    this.read = read
+}
+
+function toBookshelf() {
+    //TODO create a function that sends the created object to the 
+    bookshelf.unshift(new Book(ftitle.value, fauthor.value, fpages.value, fread.checked));
+    createElements();
+    // const newReadButton = document.createElement('button');
+    // const cardDiv = document.createElement('div');
+    // const btitleH1 = document.createElement('h1');
+    // const bauthorH2 = document.createElement('h2');
+    // const bpagesP = document.createElement('p');
+    // const actionDiv = document.createElement('div');
+    // const newRemoveButton = document.createElement('button');
+    
+    // cardDiv.className = "card";
+    
+    // btitleH1.className = "btitle";
+    // btitleH1.textContent = ftitle.value;
+    
+    // bauthorH2.className = "bauthor"; 
+    // bauthorH2.textContent = fauthor.value;
+    
+    // bpagesP.className = "bpages";
+    // bpagesP.textContent = `${fpages.value} page(s)`;
+    
+    // actionDiv.className = "action-buttons";
+
+    // if(fread.checked){
+    //     newReadButton.className = "bread";
+    //     newReadButton.classList.add("hasRead");
+    //     newReadButton.textContent = "Read it!";
+    // }
+    // if(!fread.checked){
+    //     newReadButton.className = "bread";
+    //     newReadButton.textContent = "Unread";
+    // }
+    
+    // newRemoveButton.className = "remove";
+    // newRemoveButton.textContent = "Remove";
+    
+    // cardField.appendChild(cardDiv);
+    // cardDiv.appendChild(btitleH1);
+    // cardDiv.appendChild(bauthorH2);
+    // cardDiv.appendChild(bpagesP);
+    // cardDiv.appendChild(actionDiv);
+    // actionDiv.appendChild(newReadButton);
+    // actionDiv.appendChild(newRemoveButton);
+
+    // newReadButton.addEventListener("click", function(e){
+    //     newReadButton.classList.toggle("hasRead");
+    //     if(newReadButton.textContent == "Unread"){
+    //         newReadButton.textContent = "Read it!";
+    //     }
+    //     else{newReadButton.textContent = "Unread";}
+    // });
+    
+    // newRemoveButton.addEventListener("click", function(e){
+    //     let index = bookshelf.findIndex(element => {
+    //         if(element.title === btitleH1.textContent){
+    //             return true;
+    //         }
+    //     });
+    //     cardField.removeChild(cardDiv);
+    //     bookshelf.splice(bookshelf[index], 1);
+    // });
+
+    // if(storageAvailable('localStorage')){
+    //     strBookshelf = JSON.stringify(bookshelf);
+    //     localStorage.setItem('strBookshelf', strBookshelf);
+
+    // }
 
 }
+
 
 function double(){
     for(let i = bookshelf.length - 1; i >= 0; i--){
